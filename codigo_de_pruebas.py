3d926debf2d7cbe46757a74b33291ac950bcdaa0
Rafael_Dorantes <rafaeldorantes59@gmail.com>
2025-05-19 19:56:49 -0600

    cambio de Rafael

--- codigo_de_pruebas.py
+++ codigo_de_pruebas.py
@@ -2,3 +2,5 @@ Prueba_1 = "agregaran variables con sus nombres pondran un print con su nombre b
 Nix = "hola soy Rafael Dorantes "
 print(Prueba_1)
 print(Nix)
+primer_cambio = "soy yo de nuevo jejej"
+print(primer_cambio)
